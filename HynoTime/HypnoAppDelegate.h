cbed4948ecd362442978e3dc7c7e6cd3ce056392
proudcastle <proudcastle@gmx.de>
2012-11-08 13:16:22 +0100

    First Commit

---
+++ HynoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HynoTime
+//
+//  Created by Markus Stolzenburg on 08.11.12.
+//  Copyright (c) 2012 Markus Stolzenburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HynoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HynoTime
+//
+//  Created by Markus Stolzenburg on 08.11.12.
+//  Copyright (c) 2012 Markus Stolzenburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoAppDelegate class]));
+    }
+}
